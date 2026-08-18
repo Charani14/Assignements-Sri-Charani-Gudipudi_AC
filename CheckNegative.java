5b9efaba9464bec93e1a6a3a2c26aa5d11274e98
Sri Charani Gudipudi <charanigudipudi1024@gmail.com>
2026-08-18 16:34:49 +0530

    Lab assignment-1 completed

---
+++ CheckNegative.java
@@ -0,0 +1,12 @@
+
+public class CheckNegative {
+    public static void main(String[] args) {
+        int number = -5;
+
+        if (number < 0) {
+            System.out.println(number + " is a negative number.");
+        } else {
+            System.out.println(number + " is not a negative number.");
+        }
+    }
+}
