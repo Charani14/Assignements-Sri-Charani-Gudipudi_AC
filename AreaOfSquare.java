d7bd2c61c09cd38a6768860760f9cee13c904dfd
Sri Charani Gudipudi <charanigudipudi1024@gmail.com>
2026-08-18 16:58:23 +0530

    Assignment-1 completed

--- AreaOfSquare.java
+++
@@ -1,9 +0,0 @@
-public class AreaOfSquare {
-    public static void main(String[] args) {
-        int side = 5;
-        int area = side * side;
-
-        System.out.println("Side of the square: " + side);
-        System.out.println("Area of the square: " + area);
-    }
-}
